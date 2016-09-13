b6258820732fae4a7a0b21edd4959aecf450a1b4
Andrew <andrew.avantgardian@gmail.com>
2016-09-13 17:01:44 +0200

    Update cfg.py
    removing local oauth ID settings

--- cfg.py
+++ cfg.py
@@ -5,5 +5,5 @@ HOST = "irc.twitch.tv"                          # Hostname of the IRC-Server in 
 PORT = 6667                                     # Default IRC-Port
 CHAN = "#avantgardian"                          # Channelname = #{Nickname}
 NICK = "360dog"                                 # Nickname = Twitch username
-PASS = "oauth:0uo26sob7u48p03kwbrz54jbl8iedz"   # www.twitchapps.com/tmi/ will help to retrieve the required authkey
+PASS = "oauth:oauthIDstring"   # www.twitchapps.com/tmi/ will help to retrieve the required authkey
 # --------------------------------------------- End Settings -------------------------------------------------------
